6422c8251918d66d18d451b23c3198b474a6c0d3
Your Name <you@example.com>
2024-05-01 23:27:38 -0300

    Aula2 Rotinas

---
+++ Rotinas/data/index.js
@@ -0,0 +1,45 @@
+let DATA =  [
+    {
+    id:'01',
+    porcentagem :'30%', 
+    titulo : 'Leitura/Estudos',
+    imagem :'https://uploads.metropoles.com/wp-content/uploads/2017/08/23131451/livros.jpg',
+     Descricao :'o Tempo gasto estudando e fazendo Trabalho escolares'  
+    },
+    {
+     id:'02',
+     porcentagem :'30%', 
+    titulo : 'Escola',
+     imagem :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSagxOz48gNI58ZmxAhWpepmxsuB9o-ZAkWPah-rWJ5g&s',
+     Descricao :'Basicamente a vida na Etec' 
+    },
+    {
+        id:'03',
+        porcentagem :'5%', 
+        titulo : 'Descanso',
+        imagem :'https://s1.abcstatics.com/abc/www/multimedia/economia/2023/02/10/sleep-gcc-Rb0Ia3m6tQ1Y32GLy6i2clK-1200x840@abc.png',
+        Descricao :'simplesmente Dormir e parar pra Relaxar'  
+    },
+    {
+        id:'04',
+        porcentagem :'15%', 
+        titulo : 'Espotes/Lazer',
+        imagem :'https://img.freepik.com/fotos-gratis/vista-superior-do-basquete-e-futebol_23-2148796953.jpg',
+        Descricao :'Futebol , Basquete e exercicios fisicos'  
+    },
+
+    {
+        id:'05',
+        porcentagem :'20%', 
+        titulo : 'Entretenimento',
+        imagem :'https://s2-techtudo.glbimg.com/PtuNQuS-gqQXUN6WdBSuPR9ebDk=/0x0:695x390/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/Q/t/RpKuSJTaAC6SjEfJnlBQ/xbox-network-live-gold-nome-xbox-series-x-s-one-360-windows-10.jpg',
+        Descricao :'Jogar Videogame, assitir TV e redes sociais'  
+    }
+
+
+
+
+
+]
+
+export default DATA;
